1042f7cd0e4c3c22cda93ffb768ac8ad9d5a4073
Max Eckard <eckardm@umich.edu>
2021-02-15 12:10:07 -0500

    Parses CSV spreadsheet

--- avatar.py
+++ avatar.py
@@ -1,9 +1,11 @@
 import argparse
 import configparser
+import csv
+import pathlib
 import requests
 
 parser = argparse.ArgumentParser(description='Add ArchivesSpace <dsc> elements from data in the A/V Database.')
-parser.add_argument('project_csv', help='Path to a project CSV')
+parser.add_argument('project_csv', metavar='/path/to/project/csv', type=pathlib.Path, help='Path to a project CSV')
 args = parser.parse_args()
 
 config = configparser.ConfigParser()
@@ -12,7 +14,7 @@ base_url = config['ArchivesSpace']['BaseURL']
 user = config['ArchivesSpace']['User']
 password = config['ArchivesSpace']['Password']
 
-print('authenticating')
+print('authenticating to ArchivesSpace')
 endpoint = '/users/' + user + '/login'
 params = {'password': password}
 response = requests.post(base_url + endpoint, params=params)
@@ -20,3 +22,13 @@ print(response.text)
 
 response = response.json()
 session_key = response['session']
+
+print('parsing project CSV')
+collections = []
+items = []
+parts = []
+
+results = []
+
+with open(args.project_csv, encoding='utf-8') as f:
+    reader = csv.DictReader(f)
